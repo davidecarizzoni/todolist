a2f0da0f6cb64f9f9ed465695ebf0d61d531938f
davidecarizzoni <davide.carizzoni@runelab.it>
2023-05-14 11:49:14 +0200

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Davide Carizzoni on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
